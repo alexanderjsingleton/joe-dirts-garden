cec10826ae73e910360648cf4654b3c8ac063e37
Alex Singleton <alexanderjsingleton@gmail.com>
2015-10-29 20:12:58 -0500

    finalized

--- Flower.java
+++ Flower.java
@@ -12,17 +12,61 @@ public class Flower extends Actor
      * Act - do whatever the Flower wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public static final int FLOWER_LIFE = 200;
+    private int age;
+    private int choice;
+    public Flower(int startingAge)
+    {
+        age = startingAge;
+        selectFlowerImage();
+        // int choice = Greenfoot.getRandomNumber(4); // will get 0 or 1 or 2
+        if ( choice == 0 ) 
+        {
+            setImage("flower2.png");
+        }
+        else if ( choice == 1 )
+        {
+            setImage("sunflower.png");
+        }
+        else if ( choice == 2 )
+        {
+            setImage("tulip.png");
+        }
+        else
+        {
+            setImage("ant3.png"); // bug in our program if this executes
+        }
+    }
+
     public void act() 
     {
+        age = age + 1;
         fade();
+        checkToRemoveObject();
     }   
 
-    public void jumpUpAndDown()
+    public void selectFlowerImage()
     {
-        int amount = getImage().getHeight() / 2; // height of the jump is 1/2 height of cat image height
-        setLocation(getX(),getY() - amount);
-        checkForPizza();
-        wait(10);
-        setLocation(getX(),getY() + amount);
+      
+    }
+
+    public void checkToRemoveObject()
+    {
+        if ( age == FLOWER_LIFE ) 
+        {
+            World myWorldAssistant = getWorld();
+            myWorldAssistant.removeObject(this);
+        }
+    }
+
+    public void fade()
+    {
+        if( (age % 30 ) == 0 )
+        {
+            GreenfootImage myImageAssistant = getImage();
+            int previousTransparency = myImageAssistant.getTransparency();
+            int newTransparency = (int)(((double)previousTransparency) * .8);
+            myImageAssistant.setTransparency(newTransparency); 
+        }
     }
 }
--- FlowerWorld.java
+++ FlowerWorld.java
@@ -8,6 +8,9 @@ import greenfoot.*;
  */
 public class FlowerWorld extends World
 {
+    private static final int NUMBER_FLOWERS = 8;
+    private static final int HOW_OFTEN_TO_ADD = Flower.FLOWER_LIFE / NUMBER_FLOWERS;
+    private int worldAge;
 
     /**
      * Constructor for objects of class FlowerWorld.
@@ -16,6 +19,36 @@ public class FlowerWorld extends World
     public FlowerWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        worldAge = 0;
+        populateFlowerBed();
+
+    }
+
+    public void act()
+    {
+        worldAge = worldAge + 1;
+        checkToAddFlower();
+    }
+
+
+    public void checkToAddFlower()
+    {
+        if ( (worldAge % HOW_OFTEN_TO_ADD) == 0)
+        {
+            addObject(new Flower(0), 
+                Greenfoot.getRandomNumber(getWidth()),
+                getHeight() - 30);
+        }
+    }
+
+    public void populateFlowerBed() 
+    {
+        for ( int i = 0; i < 8 ; i++)
+        {
+            addObject(new Flower(Greenfoot.getRandomNumber(Flower.FLOWER_LIFE)),
+                Greenfoot.getRandomNumber(getWidth()),
+                getHeight() - 30);
+        }
     }
 }
